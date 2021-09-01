2536e949a6b552ba2bd88bdc53a32f02ab2a852d
Andrea Pimienta <andrea_pimienta@hotmail.com>
2021-09-01 07:41:05 -0500

    Frontend

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+<head>
+    <meta charset="UTF-8">
+    <title>Vive una explosión de sabores al estilo peruano
+</head>
+<body>
+
+<div>
+    <img src="imagenes/logo.jpg" width= "auto" alt="">
+</div>
+
+</body>
+
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
